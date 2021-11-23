1e28a7d61ba1c786f0cbc562f5da09428b20d77d
mina-beiki <mina_beiki@yahoo.com>
2021-11-24 00:29:20 +0330

    added struct for shared memory

--- main.c
+++ main.c
@@ -12,20 +12,27 @@
 
 int shmid;
 
+typedef struct {
+    int ctr, read_ctr;
+    pthread_mutex_t mutex;
+    pthread_mutex_t rw_mutex;
+} shNode; //nodes of shared memory which each is a struct
+
+
 int main() {
     printf("creating the shared memory:\n");
     key_t key;
     key = 3232;
 
-    if ((shmid = shmget(IPC_PRIVATErea, SHMSIZE, IPC_CREAT | 0666)) < 0) {
+    if ((shmid = shmget(IPC_PRIVATE, sizeof(shNode), IPC_CREAT | 0666)) < 0) {
         perror("shmget");
-        exit(1);
+        exit(-1);
     }
 
     char *shm;
     if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) {
         perror("shmat");
-        exit(1);
+        exit(-1);
     }
     return 0;
 }
